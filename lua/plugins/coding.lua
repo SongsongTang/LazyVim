4ebe2931850d132189298e62be6bc17db3ed606b
Songsong Tang <tangss@mail.ustc.edu.cn>
2024-09-23 23:45:20 +0800

    tab switch, logo, copilot, etc.

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -23,7 +23,7 @@ return {
             vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
             local cmp = require("cmp")
             local defaults = require("cmp.config.default")()
-            local auto_select = true
+            local auto_select = false
             return {
                 auto_brackets = {}, -- configure any filetype to auto add brackets
                 completion = {
@@ -36,7 +36,7 @@ return {
                     ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
                     ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
                     ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<CR>"] = LazyVim.cmp.confirm({ select = auto_select }),
+                    ["<CR>"] = LazyVim.cmp.confirm({ select = false }),
                     ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
                     ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
                     ["<C-CR>"] = function(fallback)
