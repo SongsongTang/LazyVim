17509a82e667de81344605bc01fc730f50761b5f
Songsong Tang <tangss@mail.ustc.edu.cn>
2024-09-25 00:27:15 +0800

    set clang_format indent 4, with Microsoft style

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "stevearc/conform.nvim",
+        optional = true,
+        opts = {
+            formatters_by_ft = {
+                ["cpp"] = { "clang_format" },
+            },
+            formatters = {
+                clang_format = {
+                    prepend_args = { "-style=Microsoft" },
+                },
+            },
+        },
+    },
+}
