990e95fb776bf095c9e2e8e4fe8fb9e72e1dbe5c
Songsong Tang <stang1375716572@gmail.com>
2024-10-28 20:41:42 +0800

    set cursorcolumn highlight

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,4 @@
 vim.opt.shiftwidth = 4 -- Size of an indent
 vim.opt.tabstop = 4 -- Number of spaces tabs count for
 vim.opt.wrap = true -- Display long lines as just break line
+vim.opt.cursorcolumn = true -- Highlight the screen column of the cursor with CursorColumn
