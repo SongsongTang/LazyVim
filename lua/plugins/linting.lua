69c1bb661340b31b085e4ed9006ccb3b5e59381b
Songsong Tang <tangss@mail.ustc.edu.cn>
2025-01-13 10:09:40 +0800

    add linting

---
+++ lua/plugins/linting.lua
@@ -0,0 +1,47 @@
+return {
+    {
+        "mfussenegger/nvim-lint",
+        opts = {
+            -- Event to trigger linters
+            events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+            linters_by_ft = {
+                fish = { "fish" },
+                -- Use the "*" filetype to run linters on all filetypes.
+                -- ['*'] = { 'global linter' },
+                -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+                -- ['_'] = { 'fallback linter' },
+                -- ["*"] = { "typos" },
+            },
+            -- LazyVim extension to easily override linter options
+            -- or add custom linters.
+            ---@type table<string,table>
+            linters = {
+                -- -- Example of using selene only when a selene.toml file is present
+                -- selene = {
+                --   -- `condition` is another LazyVim extension that allows you to
+                --   -- dynamically enable/disable linters based on the context.
+                --   condition = function(ctx)
+                --     return vim.fs.find({ "selene.toml" }, { path = ctx.filename, upward = true })[1]
+                --   end,
+                -- },
+
+                -- not support verible_verilog_lint
+                -- verible_verilog_lint = {
+                --     sourceName = "verible-verilog-lint",
+                --     command = "verible-verilog-lint",
+                --     rootPatterns = { ".git" },
+                --     debounce = 100,
+                --     args = {
+                --         "--rules=-explicit-parameter-storage-type",
+                --         "--lint_fatal",
+                --         "--lint_warning",
+                --         "--lint_info",
+                --         "--lint_macro",
+                --         "-",
+                --     },
+                --     stdin = true,
+                -- },
+            },
+        },
+    },
+}
