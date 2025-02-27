26dce37a859564173917d1d4f068a9d499391c83
Songsong Tang <tangss@mail.ustc.edu.cn>
2025-02-27 12:18:27 +0800

    update lsp using LazyExtras

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,13 +5,7 @@ return {
         optional = true,
         opts = {
             ensure_installed = {
-                "cmake-language-server",
-                "clangd",
-                "codelldb",
-                "texlab",
                 "verible",
-                "pyright",
-                "ruff",
                 "lua-language-server",
             },
         },
@@ -20,74 +14,7 @@ return {
         "neovim/nvim-lspconfig",
         opts = {
             -- make sure mason installs the server
-            servers = {
-                jsonls = {
-                    -- lazy-load schemastore when needed
-                    on_new_config = function(new_config)
-                        new_config.settings.json.schemas = new_config.settings.json.schemas or {}
-                        vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
-                    end,
-                    settings = {
-                        json = {
-                            format = {
-                                enable = true,
-                            },
-                            validate = { enable = true },
-                        },
-                    },
-                },
-                clangd = {
-                    keys = {
-                        { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
-                    },
-                    root_dir = function(fname)
-                        return require("lspconfig.util").root_pattern(
-                            "Makefile",
-                            "configure.ac",
-                            "configure.in",
-                            "config.h.in",
-                            "meson.build",
-                            "meson_options.txt",
-                            "build.ninja"
-                        )(fname) or require("lspconfig.util").root_pattern(
-                            "compile_commands.json",
-                            "compile_flags.txt"
-                        )(fname) or require("lspconfig.util").find_git_ancestor(fname)
-                    end,
-                    capabilities = {
-                        offsetEncoding = { "utf-16" },
-                    },
-                    cmd = {
-                        "clangd",
-                        "--background-index",
-                        "--clang-tidy",
-                        "--header-insertion=iwyu",
-                        "--completion-style=detailed",
-                        "--function-arg-placeholders",
-                        "--fallback-style=llvm",
-                    },
-                    init_options = {
-                        usePlaceholders = true,
-                        completeUnimported = true,
-                        clangdFileStatus = true,
-                    },
-                },
-                ruff = {
-                    cmd_env = { RUFF_TRACE = "messages" },
-                    init_options = {
-                        settings = {
-                            logLevel = "error",
-                        },
-                    },
-                    keys = {
-                        {
-                            "<leader>co",
-                            LazyVim.lsp.action["source.organizeImports"],
-                            desc = "Organize Imports",
-                        },
-                    },
-                },
-            },
+            servers = {},
         },
     },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,17 +2,7 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         opts = function(_, opts)
-            opts.ensure_installed = { "cpp", "verilog", "json5", "ninja", "rst" }
-
-            opts.highlight = opts.highlight or {}
-            if type(opts.ensure_installed) == "table" then
-                vim.list_extend(opts.ensure_installed, { "bibtex" })
-            end
-            if type(opts.highlight.disable) == "table" then
-                vim.list_extend(opts.highlight.disable, { "latex" })
-            else
-                opts.highlight.disable = { "latex" }
-            end
+            opts.ensure_installed = { "verilog" }
         end,
     },
 }
