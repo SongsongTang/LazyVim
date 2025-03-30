7a92466dfdedb5757ea9d0f7b6b03695c07f07b5
Songsong Tang <stang1375716572@gmail.com>
2025-03-30 17:56:49 +0800

    replace zathura to sioyek

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,9 +3,12 @@ return {
     -- lazy = false, -- lazy-loading will disable inverse search
     ft = "latex",
     config = function()
-        vim.g.vimtex_view_general_viewer = "SumatraPDF"
-        --" Simpler alternatives for following option:
-        vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+        -- viewer method:
+        vim.g.vimtex_view_method = "sioyek"
+        -- Or with a generic interface:
+        -- vim.g.vimtex_view_general_viewer = "okular"
+        -- vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
+        -- vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
 
         vim.g.vimtex_compiler_latexmk = {
             aux_dir = "./out",
