2cba82184bf406dfbcda923530bf864a5a7623b2
Songsong Tang <tangss@mail.ustc.edu.cn>
2026-01-15 17:43:28 +0800

    fix opening .v file using hex bug.

--- lua/plugins/hex.lua
+++ lua/plugins/hex.lua
@@ -1,5 +1,10 @@
 return {
     "RaafatTurki/hex.nvim",
+    event = "BufReadPre",
+    cond = function()
+        local name = vim.fn.expand("%:e")
+        return name == "dat" or name == "exe"
+    end,
     opts = {
         is_file_binary = function()
             return true
